5fc9d7d39b91f36047285db973e4302df56b581a
Rosen Kadrev <102049744+rosenKadrev@users.noreply.github.com>
2022-04-06 10:06:07 +0300

    Delete Programming Fundamentals with JS - January 2022/5 . Arrays directory

--- Programming Fundamentals with JS - January 2022/5 . Arrays/01.sumFirstAndLastArrayElements.js
+++
@@ -1,7 +0,0 @@
-function sumFirstAndLastElements(input) {
-
-    let firstElement = Number(input[0]);
-    let lastElement = Number(input[input.length - 1]);
-    console.log(firstElement + lastElement);
-
-}
