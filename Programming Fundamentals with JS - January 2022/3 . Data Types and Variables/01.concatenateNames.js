49cf51d0fd3c1bc65f184e9b178f17573c5f1a6a
Rosen Kadrev <102049744+rosenKadrev@users.noreply.github.com>
2022-04-06 10:06:24 +0300

    Delete Programming Fundamentals with JS - January 2022/3 . Data Types and Variables directory

--- Programming Fundamentals with JS - January 2022/3 . Data Types and Variables/01.concatenateNames.js
+++
@@ -1,8 +0,0 @@
-function concatenateNames(name1, name2, delimiter) {
-
-    let concatenate = `${name1}${delimiter}${name2}`
-
-    console.log(concatenate);
-
-}
-concatenateNames('John', 'Smith', '->')
