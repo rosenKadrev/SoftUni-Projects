c2d9be3f132a003ea282a4a099cdd08c57cf28d8
rosenKadrev <roro_901@abv.bg>
2022-04-06 09:47:58 +0300

    Data Types and Variables

---
+++ Programming Fundamentals with JS - January 2022/03 . Data Types and Variables/05.gramophone.js
@@ -0,0 +1,10 @@
+function gramophone(band, album, song) {
+
+    let durationInSeconds = band.length * album.length * song.length / 2;
+    let rotations = Math.ceil(durationInSeconds / 2.5);
+
+    console.log(`The plate was rotated ${rotations} times.`);
+
+}
+gramophone('Black Sabbath', 'Paranoid', 'War Pigs');
+gramophone('Rammstein', 'Sehnsucht', 'Engel');
