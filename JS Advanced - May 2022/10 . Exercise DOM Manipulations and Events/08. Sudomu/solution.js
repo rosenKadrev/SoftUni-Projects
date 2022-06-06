a46ab1b1c203d9807317c613cb70cec8d2af62b1
rosenKadrev <roro_901@abv.bg>
2022-06-06 09:18:33 +0300

    Exercise DOM Manipulations and Events

---
+++ JS Advanced - May 2022/10 . Exercise DOM Manipulations and Events/08. Sudomu/solution.js
@@ -0,0 +1,50 @@
+function solve() {
+    const [checkBtn, clearBtn] = Array.from(document.querySelectorAll('#exercise>table>tfoot button'));
+    const resultElem = document.querySelector('#check>p');
+    const tableElem = document.querySelector('table');
+    const tbodyElem = document.querySelector('tbody');
+
+    function isValidLine(tr) {
+        const arr = Array.from(tr.children).map(e => e.children[0].value);
+        const unique = new Set(arr);
+        if (unique.size === 3) {
+            return true;
+        }
+        return false;
+    }
+
+    function isValidColumn(arrOfRows) {
+        for (let i = 0; i < arrOfRows[0].children.length; i++) {
+            const curr = arrOfRows.map(e => Array.from(e.children)[i]).map(td => td.children[0].value);
+            const unique = new Set(curr);
+            if (unique.size !== 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function checkHandler() {
+        const isValidRow = Array.from(tbodyElem.children).map(isValidLine).every(e => e == true);
+
+        if (isValidRow == true && isValidColumn(Array.from(tbodyElem.children)) == true) {
+            tableElem.style.border = '2px solid green';
+            resultElem.textContent = 'You solve it! Congratulations!';
+            resultElem.style.color = 'green';
+        } else {
+            tableElem.style.border = '2px solid red';
+            resultElem.textContent = 'NOP! You are not done yet...';
+            resultElem.style.color = 'red';
+        }
+    }
+
+    function clearHandler() {
+        const tdElements = document.querySelectorAll('tbody td');
+        Array.from(tdElements).map(e => e.children[0].value = '');
+        tableElem.style.border = 'none';
+        resultElem.textContent = '';
+    }
+
+    checkBtn.addEventListener('click', checkHandler);
+    clearBtn.addEventListener('click', clearHandler);
+}
---
+++ JS Advanced - May 2022/10 . Exercise DOM Manipulations and Events/09. JavaScript-Quiz/solution.js
@@ -0,0 +1,34 @@
+function solve() {
+    const sectionElements = document.querySelectorAll('#quizzie section');
+    const resultElem = document.querySelector('.results-inner>h1');
+
+    if (sectionElements == null || resultElem == null) {
+        throw new Error('Missing DOM elements!');
+    }
+
+    const answers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
+    let answersCount = 0;
+
+    function clickHandler(e) {
+        if (e.target.className == 'answer-text') {
+            if (answers.includes(e.target.textContent) == true) {
+                answersCount += 1;
+            }
+            e.currentTarget.style.display = 'none';
+
+            if (e.currentTarget.nextElementSibling != null) {
+                e.currentTarget.nextElementSibling.style.display = 'block';
+            }
+
+            if (e.currentTarget.nextElementSibling.id == 'results') {
+                if (answersCount == answers.length) {
+                    resultElem.textContent = 'You are recognized as top JavaScript fan!';
+                } else {
+                    resultElem.textContent = `You have ${answersCount} right answers`;
+                }
+            }
+        }
+    }
+
+    Array.from(sectionElements).forEach(e => e.addEventListener('click', clickHandler));
+}
